d557d80b8b230d44bbb59284018c254fc45a6efb
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-20 20:08:28 +0530

    req.send and body-parser express.js demo

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("in the middlewarer");
-    next();// allows request to continue to next middleware
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+    res.send("<form action='/product' method='POST'><lable for='title'>Product Title:</lable><input type='text' name='title'/><br><lable for='size'>Product Size:</lable><input type='text' name='size'/><br><button type='submit'>Add Product</button></form>");
+});
+//app.get for get request and app.post for post request
+app.post('/product', (req, res, next) => {
+    console.log(req.body.title);
+    console.log(req.body.size);
+    res.redirect('/');
+});
+app.use("/", (req, res, next) => {
+    res.send("<h1>Hello from Express</h1>");
+});
 
-app.use((req, res, next) => {
-    console.log("in  another the middlewarer");
-    res.send({ key1: "value" });
-})
-
-app.listen(4000)
+app.listen(4000);
